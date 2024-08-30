55d607d0a4d23d1a2c0e05a68b24742de26a8b2e
DinohRatiarisandy <dinohratiarisandy@gmail.com>
2024-08-30 16:43:47 +0100

    refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
 import { ThemeProvider } from "@/components/utils/theme-provider";
-import ChatList from "./components/section-chat-list/ChatList";
-import ChatDetails from "./components/section-chat-details/ChatDetails";
-import ChatMedia from "./components/section-media-shared/ChatMedia";
+import AppContent from "./components/AppContent";
+import { AuthProvider } from "./components/context/AuthProvider";
 
 function App() {
    return (
       <ThemeProvider defaultTheme="light" storageKey="ui-theme">
-         <div className="flex">
-            <ChatList className="flex h-[100vh] flex-[1] flex-col gap-2 border-r border-accent" />
-            <ChatDetails className="flex h-[100vh] flex-[2.5] flex-col border-accent lg:border-r" />
-            <ChatMedia className="hidden h-[100vh] flex-[1] flex-col lg:flex" />
-         </div>
+         <AuthProvider>
+            <AppContent />
+         </AuthProvider>
       </ThemeProvider>
    );
 }
