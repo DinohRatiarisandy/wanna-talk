da02143a1b795fa0aff10333340c6aa02c76533e
DinohRatiarisandy <dinohratiarisandy@gmail.com>
2024-08-31 18:58:15 +0100

    search existed user to start chat

---
+++ src/components/section-chat-list/NewChat.tsx
@@ -0,0 +1,77 @@
+import { Search, UserRoundPen } from "lucide-react";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import {
+   collection,
+   DocumentData,
+   getDocs,
+   query,
+   where,
+} from "firebase/firestore";
+import { database } from "@/firebase";
+import { useState } from "react";
+import UserInfo from "./UserInfo";
+
+export default function NewChat() {
+   const [friends, setFriends] = useState<DocumentData | null>(null);
+
+   async function handleSearch(e: React.FormEvent<HTMLFormElement>) {
+      e.preventDefault();
+
+      const form = e.currentTarget;
+      const usernameInput = form.elements.namedItem(
+         "username",
+      ) as HTMLInputElement;
+
+      try {
+         const userRef = collection(database, "users");
+         const q = query(userRef, where("userName", "==", usernameInput.value));
+         const querySnapShot = await getDocs(q);
+
+         if (!querySnapShot.empty) {
+            setFriends(querySnapShot.docs[0].data());
+         }
+      } catch (error) {
+         console.log(error);
+      }
+   }
+
+   return (
+      <Popover>
+         <PopoverTrigger asChild>
+            <UserRoundPen size={24} className="cursor-pointer" />
+         </PopoverTrigger>
+         <PopoverContent className="ml-2 flex w-80 flex-col gap-4 p-2">
+            <form
+               onSubmit={(e) => handleSearch(e)}
+               className="flex items-center rounded border p-1"
+            >
+               <Search size={24} className="text-muted-foreground" />
+               <Input
+                  id="username"
+                  type="text"
+                  placeholder="Search user"
+                  className="h-8 border-none focus-visible:ring-0 focus-visible:ring-offset-0"
+               />
+            </form>
+            {/*
+             * Results
+             */}
+            <div className="flex flex-col">
+               {Boolean(friends) && (
+                  <div className="flex items-center justify-between">
+                     <UserInfo
+                        key={friends?.userID}
+                        userName={friends?.userName}
+                        userProfil={friends?.userProfil}
+                        className="flex items-center gap-2"
+                     />
+                     <Button variant="outline">Add</Button>
+                  </div>
+               )}
+            </div>
+         </PopoverContent>
+      </Popover>
+   );
+}
