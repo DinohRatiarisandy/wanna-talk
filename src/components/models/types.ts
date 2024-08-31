4d9ceb5fb85a8b90e2b4df5b7d489acc4b2d95b1
DinohRatiarisandy <dinohratiarisandy@gmail.com>
2024-08-31 18:55:19 +0100

    userFirebase type

---
+++ src/components/models/types.ts
@@ -0,0 +1,6 @@
+export type UserInfos = {
+   userID: string | undefined;
+   userName: string | undefined | null;
+   userEmail: string | undefined | null;
+   userProfil: string | undefined | null;
+};
