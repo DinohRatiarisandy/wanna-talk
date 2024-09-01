2c6d2af90f18232b7732fabbddb1785d7dff95d8
DinohRatiarisandy <dinohratiarisandy@gmail.com>
2024-09-01 15:37:09 +0100

    feat: add fetch function to fetch userinfo

--- src/store/useAuthStore.ts
+++ src/store/useAuthStore.ts
@@ -1,18 +1,35 @@
 import { create } from "zustand";
 import { UserFirebase } from "@/components/models/types";
+import { doc, getDoc } from "firebase/firestore";
+import { database } from "@/firebase";
 
 type AuthState = {
    user: UserFirebase | undefined | null;
    isLoading: boolean;
    setUser: (user: UserFirebase | undefined | null) => void;
-   setLoading: (loading: boolean) => void;
+   fetchUserInfo: (uid: string) => Promise<void>;
 };
 
-export const useAuthStore = create<AuthState>(function (set) {
-   return {
-      user: null,
-      isLoading: true,
-      setUser: (user) => set({ user, isLoading: false }),
-      setLoading: (loading) => set({ isLoading: loading }),
-   };
-});
+export const useAuthStore = create<AuthState>((set) => ({
+   user: null,
+   isLoading: true,
+   setUser: (user) => set({ user, isLoading: false }),
+   fetchUserInfo: async (uid: string) => {
+      if (!uid) {
+         set({ user: null, isLoading: false });
+         return;
+      }
+      try {
+         const docRef = doc(database, "users", uid);
+         const docSnap = await getDoc(docRef);
+
+         if (docSnap.exists()) {
+            set({ user: docSnap.data() as UserFirebase, isLoading: false });
+         } else {
+            set({ user: null, isLoading: false });
+         }
+      } catch (error) {
+         set({ user: null, isLoading: false });
+      }
+   },
+}));
