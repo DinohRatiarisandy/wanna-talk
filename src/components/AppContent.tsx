91f967aab385a2ea94da67574af0babb8637d5bd
DinohRatiarisandy <dinohratiarisandy@gmail.com>
2024-09-01 15:25:40 +0100

    fix: fetch user info after page reload

--- src/components/AppContent.tsx
+++ src/components/AppContent.tsx
@@ -6,24 +6,51 @@ import { useAuthStore } from "@/store/useAuthStore";
 import { useThemeStore } from "@/store/useThemeStore";
 import { useEffect } from "react";
 import Loading from "./ui/Loading";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "@/firebase";
 
 export default function AppContent() {
-   const { user, isLoading } = useAuthStore();
+   const { user, isLoading, fetchUserInfo, setUser } = useAuthStore();
    const { theme } = useThemeStore();
+
+   useEffect(() => {
+      const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+         if (currentUser?.uid) {
+            fetchUserInfo(currentUser.uid);
+         } else {
+            setUser(null);
+         }
+      });
+
+      return () => unsubscribe();
+   }, [fetchUserInfo]);
+
+   useEffect(() => {
+      if (user?.userID) {
+         fetchUserInfo(user.userID);
+      } else {
+         setUser(null);
+      }
+   }, [user?.userID, fetchUserInfo]);
 
    useEffect(() => {
       document.documentElement.classList.add(theme);
    }, [theme]);
 
+   console.log(user);
+   console.log("Loading ", isLoading);
+
    if (isLoading) return <Loading />;
 
-   return user?.userID !== undefined ? (
+   return user ? (
       <div className={`flex ${theme}`}>
          <ChatList className="flex h-[100vh] flex-[1] flex-col gap-2 border-r border-accent" />
          <ChatDetails className="flex h-[100vh] flex-[2.5] flex-col border-accent lg:border-r" />
          <ChatMedia className="hidden h-[100vh] flex-[1] flex-col lg:flex" />
       </div>
    ) : (
-      <Login />
+      <div className="flex h-[100vh] w-full items-center">
+         <Login />
+      </div>
    );
 }
