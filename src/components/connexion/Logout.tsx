c792bd0ea225a5e3331b0efae6d2bcfee9dd6304
DinohRatiarisandy <dinohratiarisandy@gmail.com>
2024-09-07 22:58:10 +0100

    fix: chatId = null if logout

--- src/components/connexion/Logout.tsx
+++ src/components/connexion/Logout.tsx
@@ -1,17 +1,23 @@
 import { auth } from "@/firebase";
 import { DropdownMenuItem } from "../ui/dropdown-menu";
 import { LogOut } from "lucide-react";
+import { useAuthStore } from "@/store/useAuthStore";
+import { useChatStore } from "@/store/useChatStore";
 
 export default function Logout() {
+   const { user } = useAuthStore();
+   const setChatId = useChatStore((state) => state.setChatId);
    return (
       <DropdownMenuItem
          onClick={async () => {
             await auth.signOut();
+            setChatId(undefined);
          }}
          className="flex cursor-pointer gap-1"
       >
          <LogOut size={18} />
-         <span>Logout</span>
+         <span>Logout:</span>
+         <i>{user?.userName}</i>
       </DropdownMenuItem>
    );
 }
