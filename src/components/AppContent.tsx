539f1bcb2b6c76420d03585d2b3da1f3c17d91c3
DinohRatiarisandy <dinohratiarisandy@gmail.com>
2024-09-07 22:56:34 +0100

    fix: add a visuel for user's profil

--- src/components/AppContent.tsx
+++ src/components/AppContent.tsx
@@ -8,10 +8,12 @@ import { useEffect } from "react";
 import Loading from "./ui/Loading";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "@/firebase";
+import { useChatStore } from "@/store/useChatStore";
 
 export default function AppContent() {
    const { user, isLoading, fetchUserInfo, setUser } = useAuthStore();
    const { theme } = useThemeStore();
+   const { chatId } = useChatStore();
 
    useEffect(() => {
       const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
@@ -37,16 +39,33 @@ export default function AppContent() {
       document.documentElement.classList.add(theme);
    }, [theme]);
 
-   console.log(user);
-   console.log("Loading ", isLoading);
-
    if (isLoading) return <Loading />;
 
    return user ? (
       <div className={`flex ${theme}`}>
          <ChatList className="flex h-[100vh] flex-[1] flex-col gap-2 border-r border-accent" />
-         <ChatDetails className="flex h-[100vh] flex-[2.5] flex-col border-accent lg:border-r" />
-         <ChatMedia className="hidden h-[100vh] flex-[1] flex-col lg:flex" />
+         {chatId ? (
+            <ChatDetails className="flex h-[100vh] flex-[2.5] flex-col border-accent lg:border-r" />
+         ) : (
+            <div className="flex h-[100vh] w-full flex-[2.5] items-center justify-center border-accent lg:border-r">
+               <div className="flex flex-col items-center justify-center gap-4">
+                  <img
+                     className="h-24 w-24 rounded-full object-cover ring ring-primary"
+                     src={user.userProfil ? user.userProfil : ""}
+                     alt={`${user.userName}'s profil`}
+                  />
+                  <p className="text-2xl">Welcome {user.userName} !</p>
+                  <p className="text-secondary-foreground">
+                     Select a conversation to chat !
+                  </p>
+               </div>
+            </div>
+         )}
+         {chatId ? (
+            <ChatMedia className="hidden h-[100vh] flex-[1] flex-col lg:flex" />
+         ) : (
+            <div className="hidden h-[100vh] flex-[1]"></div>
+         )}
       </div>
    ) : (
       <div className="flex h-[100vh] w-full items-center">
