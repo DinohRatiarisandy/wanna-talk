bb465c2835b1402607368db12415d244ccf5e6ea
DinohRatiarisandy <dinohratiarisandy@gmail.com>
2024-09-07 23:02:02 +0100

    feat: show the chat selected

--- src/components/section-chat-list/ChatCard.tsx
+++ src/components/section-chat-list/ChatCard.tsx
@@ -1,19 +1,32 @@
 import { UserFirebase } from "../models/types";
 import UserInfo from "./UserInfo";
+import { useChatStore } from "@/store/useChatStore";
+import { ChatType } from "./ChatList";
 
 type ChatCardProps = UserFirebase & {
+   chat: ChatType;
    lastMessage: string;
+   className: string;
 };
 
-export default function ChatCard(props: ChatCardProps) {
+export default function ChatCard({ className, ...props }: ChatCardProps) {
+   const { changeChat } = useChatStore();
+
+   async function handleSelect(chat: ChatType) {
+      changeChat(chat.chatId, chat.user);
+   }
+
    return (
-      <div className="flex cursor-pointer transition-all hover:bg-secondary">
+      <div
+         onClick={() => handleSelect(props.chat)}
+         className={`flex cursor-pointer transition-all hover:bg-secondary ${className}`}
+      >
          <UserInfo
             userName={props.userName}
             userProfil={props.userProfil}
             className="flex items-center gap-2 lg:text-lg"
          >
-            <p className="h-5 w-full overflow-hidden text-sm">
+            <p className="h-5 w-full overflow-hidden text-sm text-secondary-foreground">
                {props.lastMessage}
             </p>
          </UserInfo>
